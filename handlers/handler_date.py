6af7d3c77d19eab93348773a0d848f9567d9022b
Nikolay Korkushko <kakashonochka12@gmail.com>
2025-10-22 01:19:08 +0300

    create tg_bot

--- handlers/handler_date.py
+++ handlers/handler_date.py
@@ -1,11 +1,10 @@
 import logging
 import os
 
-from aiogram import Bot, Dispatcher, types, F
+from aiogram import Bot, Dispatcher, F, types
 from aiogram.filters.command import Command
 from aiogram.fsm.context import FSMContext
-from aiogram.fsm.state import StatesGroup, State
-
+from aiogram.fsm.state import State, StatesGroup
 from dotenv import load_dotenv
 
 from src.aggr_alg import save_in_json
@@ -22,6 +21,7 @@ dp = Dispatcher()
 date_from = "2022-09-01T00:00:00"
 date_to = "2022-12-31T23:59:00"
 
+
 # Хэндлер на команду /start
 @dp.message(Command("start"))
 async def main_menu(message: types.Message):
@@ -31,34 +31,38 @@ async def main_menu(message: types.Message):
     ]
 
     # Показываем список
-    text = ("Добрый день! Я ваш личный помощник в просмотре информации о зарплатах сотрудников компании."
-            f"\n\nИнтервал дат по умолчанию:\n{date_from} - {date_to}\n\n"
-            "Что вас интересует:\n\n") + "\n".join([f[0] for f in filters])
+    text = (
+        "Добрый день! Я ваш личный помощник в просмотре информации о зарплатах сотрудников компании."
+        f"\n\nИнтервал дат по умолчанию:\n{date_from} - {date_to}\n\n"
+        "Что вас интересует:\n\n"
+    ) + "\n".join([f[0] for f in filters])
     await message.answer(text)
+
 
 class DateStates(StatesGroup):
     waiting_date_from = State()
     waiting_date_to = State()
 
+
 async def show_filter_options(message: types.Message):
     kb = [
-        [types.KeyboardButton(text="по дням"),
-        types.KeyboardButton(text="по месяцам"),
-        types.KeyboardButton(text="по годам"),
-        types.KeyboardButton(text="вернуться назад"),]
+        [
+            types.KeyboardButton(text="по дням"),
+            types.KeyboardButton(text="по месяцам"),
+            types.KeyboardButton(text="по годам"),
+            types.KeyboardButton(text="вернуться назад"),
+        ]
     ]
     keyboard = types.ReplyKeyboardMarkup(
         keyboard=kb,
         resize_keyboard=True,
         selective=True,
     )
-    await message.answer(
-        "Выберите, как хотите отфильтровать:",
-        reply_markup=keyboard
-    )
+    await message.answer("Выберите, как хотите отфильтровать:", reply_markup=keyboard)
+
 
 # хэндлер для получения дат, отсортированных по умолчанию: 2022-09-01T00:00:00 - 2022-12-31T23:59:00,
-@dp.message(F.text.regexp(r'^[12]$'))
+@dp.message(F.text.regexp(r"^[12]$"))
 async def handle_menu_selection(message: types.Message, state: FSMContext):
     choice = message.text
 
@@ -67,10 +71,12 @@ async def handle_menu_selection(message: types.Message, state: FSMContext):
     elif choice == "2":
         await start_date_change(message, state)
 
+
 @dp.message(F.text == "2")
 async def start_date_change(message: types.Message, state: FSMContext):
     await message.answer("Введите начальную дату (YYYY-MM-DDTHH:MM:SS):")
     await state.set_state(DateStates.waiting_date_from)
+
 
 @dp.message(DateStates.waiting_date_from)
 async def process_date_from(message: types.Message, state: FSMContext):
@@ -78,17 +84,19 @@ async def process_date_from(message: types.Message, state: FSMContext):
     await message.answer("Введите конечную дату:")
     await state.set_state(DateStates.waiting_date_to)
 
+
 @dp.message(DateStates.waiting_date_to)
 async def process_date_to(message: types.Message, state: FSMContext):
     global date_from, date_to
 
     data = await state.get_data()
-    date_from = data['date_from']  # Меняем глобальную переменную
+    date_from = data["date_from"]  # Меняем глобальную переменную
     date_to = message.text  # Меняем глобальную переменную
 
     await message.answer(f"✅ Даты обновлены!\n{date_from} - {date_to}")
     await state.clear()
     await main_menu(message)  # Возвращаем в главное меню
+
 
 async def date_filter(message: types.Message, group_type: str):
     kb = [
@@ -107,25 +115,26 @@ async def date_filter(message: types.Message, group_type: str):
     data_result = save_in_json(date_from, date_to, group_type)
 
     await message.reply(f"📊 Результат:\n{data_result}")
-    await message.answer(
-        "выберите, как хотите отфильтровать",
-        reply_markup=keyboard
-    )
+    await message.answer("выберите, как хотите отфильтровать", reply_markup=keyboard)
+
 
 # хэндлер для получения дат, отсортированных по дням
 @dp.message(F.text.lower() == "по дням")
 async def sort_to_days(message: types.Message):
     await date_filter(message, "day")
 
+
 # хэндлер для получения дат, отсортированных по месяцам
 @dp.message(F.text.lower() == "по месяцам")
 async def sort_to_month(message: types.Message):
     await date_filter(message, "month")
 
+
 # хэндлер для получения дат, отсортированных по годам
 @dp.message(F.text.lower() == "по годам")
 async def sort_to_year(message: types.Message):
     await date_filter(message, "year")
+
 
 @dp.message(F.text.lower() == "вернуться назад")
 async def go_to_main(message: types.Message):
--- run_bot.py
+++ run_bot.py
@@ -1,12 +1,12 @@
 import asyncio
 
-from handlers.handler_date import dp
-from handlers.handler_date import bot
+from handlers.handler_date import bot, dp
 
 
 # Запуск процесса поллинга новых апдейтов
 async def main():
     await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
